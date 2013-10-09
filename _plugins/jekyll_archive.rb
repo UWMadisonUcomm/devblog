919c3d8ad3429372839695ca5f4fb366dec9d15c
Nick Weaver <jnweaver@wisc.edu>
2013-10-09 12:27:47 -0500

    Replace taxonomies plugin with latest jekyll_archive gem code and edit config as needed

--- _plugins/jekyll_archive.rb
+++ _plugins/jekyll_archive.rb
@@ -5,7 +5,7 @@
 
 module Jekyll
 
-  class TaxonomyPage < Page
+  class ArchivePage < Page
     def initialize(site, base, dir, obj, type)
       @site = site
       @base = base
@@ -16,39 +16,41 @@ module Jekyll
               when 'tag'
                 {
                   :posts => site.tags[obj],
-                  :page_title_prefix => 'Tag: '
+                  :page_title_prefix => site.config["tag_title_prefix"] || 'Tag: '
                 }
               when 'category'
                 {
                   :posts => site.categories[obj],
-                  :page_title_prefix => 'Category: '
+                  :page_title_prefix => site.config["category_title_prefix"] || 'Category: '
                 }
               end
 
+      layout_file = site.config["archive_layout"].nil? ? 'archive_page.html' : site.config["archive_layout"] + '.html'
+
       self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'archive_page.html')
+      self.read_yaml(File.join(base, '_layouts'), layout_file)
 
       self.data['posts'] = page_data[:posts]
       prefix = page_data[:page_title_prefix]
-      self.data['header'] = "#{prefix}#{obj}"
+      self.data['header'] = "#{prefix}#{obj.capitalize }"
 
       self.data['title'] = "#{self.data['header']}: "
     end
   end
 
-  class TaxonomyPageGenerator < Generator
+  class ArchivePageGenerator < Generator
     safe true
 
     def generate(site)
-      if site.layouts.key? 'archive_page'
-        dir = site.config["category_dir"] || 'category'
+      if site.layouts.key? site.config["archive_layout"] || 'archive_page'
+        dir = site.config["category_dir_name"] || 'category'
         site.categories.keys.each do |category|
-          site.pages << TaxonomyPage.new(site, site.source, File.join(dir, category), category, 'category')
+          site.pages << ArchivePage.new(site, site.source, File.join(dir, category), category, 'category')
         end
 
-        dir = site.config["tag_dir"] || 'tag'
+        dir = site.config["tag_dir_name"] || 'tag'
         site.tags.keys.each do |tag|
-          site.pages << TaxonomyPage.new(site, site.source, File.join(dir, tag), tag, 'tag')
+          site.pages << ArchivePage.new(site, site.source, File.join(dir, tag), tag, 'tag')
         end
       end
     end
